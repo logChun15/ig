c2b78ca23ed69f7460ef79d2490bdc5ce09d3d69
luochun <luochun15@gmail.com>
2022-05-05 22:15:33 +0800

    社交统计信息

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -16,9 +16,9 @@
           @include('shared._user_info', ['user' => Auth::user()])
         </section>
 
-        {{-- <section class="stats mt-2">
+        <section class="stats mt-2">
             @include('shared._stats', ['user' => Auth::user()])
-        </section> --}}
+        </section>
 
       </aside>
     </div>
