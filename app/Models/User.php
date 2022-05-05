47fea2f4020ef278c69cbad3bd8e64b0d720ad1c
luochun <luochun15@gmail.com>
2022-05-05 19:07:47 +0800

    用户激活

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
@@ -53,4 +54,13 @@ class User extends Authenticatable
     // 4. 用 strtolower 方法将邮箱转换为小写；
     // 5. 将小写的邮箱使用 md5 方法进行转码；
     // 6. 将转码后的邮箱与链接、尺寸拼接成完整的 URL 并返回；
+
+    public static function boot()
+    {
+    parent::boot();
+    static::creating(function ($user) {
+    $user->activation_token = Str::random(30);
+    });
+    }
+
 }
